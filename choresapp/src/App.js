bc99f77cbf84441ba8e50df1e42a1598e7c7d781
kidus yilma <kidusyilma@gmail.com>
2021-03-16 11:05:14 -0700

    added route

--- choresapp/src/App.js
+++ choresapp/src/App.js
@@ -1,16 +1,42 @@
 
 import React from 'react';
-import {Route} from 'react-router-dom';
-import  Login from './routes/login/login'
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './routes/landingPage/landingPage'
+import Login from './routes/login/login'
+import Signup from './routes/signup/signup'
+import Dashboard from './routes/dashboard/dashboard'
+import Homepage from './routes/homepage/homePage'
 import "./App.css";
 
-function App() {
-    return (<div className = "App" >
-    
-    <Route  exact path='/login'  component={props=><Login {...props}/>} />
-    
-    
-     </div>);
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+
+
+
+    render() {
+
+        return (<div className="App" >
+
+            <Switch>
+                <Route exact path='/login' component={props => <Login {...props} />} />
+                <Route exact path='/landingpage' component={props => <LandingPage {...props} />} />
+                <Route exact path='/signup' component={props => <Signup {...props} />} />
+                <Route exact path='/dashboard' component={props => <Dashboard {...props} />} />
+                <Route exact path='/homepage' component={props => <Homepage {...props} />} />
+                <Route   component={props=><div> <h1>Page Not found</h1> </div>} />
+
+            </Switch>
+
+
+        </div>);
+    }
 }
 
 export default App;
--- choresapp/src/routes/signup/signup.js
+++ choresapp/src/routes/signup/signup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "../../components/forms/loginForm";
+
 
 function signup() {
     return (< div >
