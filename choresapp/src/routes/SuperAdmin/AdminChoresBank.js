d9daab28826f2b8350a05f2a831c94b13ecd8900
Andrea Pullen <apullenb@gmail.com>
2021-03-23 14:09:46 -0500

    admin dash

--- choresapp/src/routes/SuperAdmin/AdminChoresBank.js
+++ choresapp/src/routes/SuperAdmin/AdminChoresBank.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function AdminChoresBank() {
+function AdminChoresBank(props) {
+    const chore = props.chore
     return (
         <div>
-            
+            ID: 0{chore.chore_opt_id}
+             <h3>Title: {chore.title } | Token Value: {chore.value}</h3>
+         <p>Description: {chore.description }</p>
+         <p>Steps: {chore.steps}</p>
+         <p>Estimated Time to Complete: {chore.time_est} minutes</p>
+         <p>Suggested Age: {chore.suggested_age}</p>
         </div>
     )
 }
