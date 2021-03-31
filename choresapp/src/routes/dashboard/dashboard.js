99c7c25bcbe19c8ba35bc832ee45df389b39e997
Andrea Pullen <apullenb@gmail.com>
2021-03-31 15:45:49 -0500

    register route

--- choresapp/src/routes/dashboard/dashboard.js
+++ choresapp/src/routes/dashboard/dashboard.js
@@ -5,15 +5,20 @@ import Button from '../../components/button/button';
 import { useDispatch } from 'react-redux'
 
 function Dashboard(props) {
-    const state = useSelector(state => state)
-    const dispatch = useDispatch();
+    
 
-    console.log(state);
+   
     return (
         <div>
             <h1>Dashboard</h1>
-            <Button background={'red'} width={"50px"} buttonName={"log out"} onclick={() => {
-                
+            
+        </div>
+    )
+}
+{/* 
+    const state = useSelector(state => state)
+    const dispatch = useDispatch();
+    <Button background={'red'} width={"50px"} buttonName={"log out"} onclick={() => {                
                 dispatch({
                 type: "LogOutUser",
                 payload: {
@@ -22,13 +27,7 @@ function Dashboard(props) {
                     isAdmin: false
                 }
             })
-            
-
-                props.history.push('/login')
-            
-            }} />
-        </div>
-    )
-}
+             props.history.push('/login')
+            }} /> */}
 
 export default withRouter(Dashboard)
